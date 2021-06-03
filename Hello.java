be084eb4131786e94c9eb43468b6d32494644506
fernandoandrade <fcatavares@gmail.com>
2021-06-02 21:55:36 -0700

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ class Hello {
 
     public static void main(String[] args) {
 
-      System.out.println("This is a java app created using Docker");
+      System.out.println("This is a java app created using Docker2");
     }
   }
